c39c0de7c1324f84264b0808ee5b1168874dd4b1
Martin Alge <martin@alge.se>
2019-01-23 14:53:00 +0100

    added more endpoints

--- ktk-phone.py
+++ ktk-phone.py
@@ -14,11 +14,11 @@ def send_media(path):
 @app.route('/new-call', methods=['POST'])
 def newCall():
   out = {
-    "ivr": "{}/media/main-menu.wav".format(base_url),
+    "ivr": "{}/media/main-menu.wav".format(config.base_url),
     "digits": 1, 
     #"timeout": 10,
     "repeat": 3,
-    "next": "{}/handle-welcome-ivr".format(base_url)
+    "next": "{}/handle-welcome-ivr".format(config.base_url)
   }
   return json.dumps(out) 
 
@@ -28,12 +28,13 @@ def handleWelcome():
   
   data = request.form
   result = data["result"]
-  print(result) 
+  print(result)
+
   if result == None:
     return "Failed"
 
   out = {
-    "next":   "{}/new-call".format(base_url)
+    "next":   "{}/new-call".format(config.base_url)
   }  
 
   if result == "0":
@@ -41,29 +42,39 @@ def handleWelcome():
 
   elif result == "1":
     out = {
-      "play":   "{}/media/ordf.wav".format(base_url),
+      "play":   "{}/media/ordf.wav".format(config.base_url),
       "next":   json.dumps({"connect":config.ordf_number})   
     }
 
   elif result == "2":
     out = {
-      "play":   "{}/media/ordf.wav".format(base_url),
+      "play":   "{}/media/ordf.wav".format(config.base_url),
       "next":   json.dumps({"connect":config.ordf_number})   
     }
 
   elif result == "3":
-    pass
+    out = {
+      "ivr": "{}/media/pick_a_note.wav".format(config.base_url),
+      "digits": 2, 
+      #"timeout": 10,
+      "repeat": 3,
+      "next": "{}/handle-note-ivr".format(config.base_url)
+    }
 
   elif result == "4":
     pass
 
   elif result == "5":
-    pass
+    out = {
+      "ivr": "{}/media/pick_a_song.wav".format(config.base_url),
+      "digits": 2, 
+      #"timeout": 10,
+      "repeat": 3,
+      "next": "{}/handle-song-ivr".format(config.base_url)
+    }
 
   elif result == "6":
-    out = {
-      "play":   "{}/media/osquar.wav".format(base_url)
-    }
+    pass
 
   elif result == "7":
     pass
@@ -76,6 +87,121 @@ def handleWelcome():
   
   return json.dumps(out)
 
+@app.route('/handle-note-ivr', methods=['POST'])
+def handleNote():
+  
+  data = request.form
+  result = int(data["result"])
+  print(result) 
+  
+  notes = {
+    1 : "a-5s.wav",
+    2 : "aiss-5s.wav",
+    3 : "b-5s.wav",
+    4 : "c-5s.wav",
+    5 : "ciss-5s.wav",
+    6 : "d-5s.wav", 
+    7 : "diss-5s.wav", 
+    8 : "e-5s.wav", 
+    9 : "f-5s.wav", 
+    10 : "fiss-5s.wav", 
+    11 : "g-5s.wav", 
+    12 : "giss-5s.wav"
+  }
+
+  if result not in notes:
+    out = {
+      "ivr": "{}/media/pick_a_note.wav".format(config.base_url),
+      "digits": 2, 
+      #"timeout": 10,
+      "repeat": 3,
+      "next": "{}/handle-note-ivr".format(config.base_url)
+    }
+    return json.dumps(out)
+
+
+  if result == None:
+    return "Failed"
+    
+  out = {
+    "play" : config.base_url+"/media/notes/"+notes[result], 
+    "next" : {
+      "play" : config.base_url+"/media/notes/"+notes[result], 
+      "next" : {
+        "play" : config.base_url+"/media/notes/"+notes[result]
+      } 
+    }
+  }  
+  
+  return json.dumps(out)
+
+
+
+@app.route('/handle-pagenote-ivr', methods=['POST'])
+def handlePageNote():
+  data = request.form
+  result = int(data["result"])
+  print(result) 
+  
+  songs = {
+    1 : "a-5s.wav",
+    2 : "aiss-5s.wav",
+    3 : "b-5s.wav"
+  }
+
+  if result not in songs:
+    out = {
+      "ivr": "{}/media/pick_a_song.wav".format(config.base_url),
+      "digits": 2, 
+      #"timeout": 10,
+      "repeat": 3,
+      "next": "{}/handle-song-ivr".format(config.base_url)
+    }
+    return json.dumps(out)
+
+
+  if result == None:
+    return "Failed"
+    
+  out = {
+    "play" : config.base_url+"/media/songs/"+songs[result], 
+  }  
+  
+  return json.dumps(out)
+
+
+@app.route('/handle-song-ivr', methods=['POST'])
+def handleSong():
+  
+  data = request.form
+  result = int(data["result"])
+  print(result) 
+  
+  songs = {
+    1 : "a-5s.wav",
+    2 : "aiss-5s.wav",
+    3 : "b-5s.wav"
+  }
+
+  if result not in songs:
+    out = {
+      "ivr": "{}/media/pick_a_song.wav".format(config.base_url),
+      "digits": 2, 
+      #"timeout": 10,
+      "repeat": 3,
+      "next": "{}/handle-song-ivr".format(config.base_url)
+    }
+    return json.dumps(out)
+
+
+  if result == None:
+    return "Failed"
+    
+  out = {
+    "play" : config.base_url+"/media/songs/"+songs[result], 
+  }  
+  
+  return json.dumps(out)
 
 @app.route('/')
 def entry_point():
